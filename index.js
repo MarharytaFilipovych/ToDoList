d9f4e348de1e416b3709ac21a8b1ad950129eaf6
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-01-14 17:11:54 +0200

    ...

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
-function addNewFieldForItem(){
-    const list = document.querySelector('.todo-list');
-    const li = document.createElement('li');
-    li.innerHTML=`<input type="checkbox" >
-            <input type="text" name="to-do" placeholder="type your to-do" value="" >`
-    changeSizeOfListForm(true);
-    list.appendChild(li);
+import {listForm} from "./listForm.js";
+
+function saveToLocalStorage(){
+    localStorage.setItem('items', JSON.stringify(myListForm.getValues()));
 }
 
-function changeSizeOfListForm(increase){
-    const listForm = document.querySelector('.list');
-    const height =listForm.offsetHeight;
-    listForm.style.height= increase?`${height+100}px`: `${height-100}px`;
-}
-
-function getHeightOfListForm(){
-    const listForm = document.querySelector('.list');
-    return listForm.offsetHeight;
-}
-
-function updateValueInItem(tag){
-    const newItem = tag.value;
-   tag.setAttribute('value', newItem);
-}
-
-function removeItem(tag){
-    if(getHeightOfListForm()>100){
-        tag.parentElement.remove();
-        changeSizeOfListForm(false);
-    }else{
-        const textInput = tag.parentElement.querySelector('input[type="text"]');
-        textInput.value = "";
-    }
-}
-
-const LIST = document.querySelector('.todo-list');
+const myListForm = new listForm();
 const addButton = document.querySelector('.plus');
 
 addButton.addEventListener('click', ()=>{
-    addNewFieldForItem();
+    myListForm.addField("");
+});
+
+window.addEventListener('beforeunload', () => {
+    saveToLocalStorage();
+});
+
+window.addEventListener('load',()=>{
+    const retrievedItems = JSON.parse(localStorage.getItem('items'));
+    myListForm.removeFirstField();
+    if(retrievedItems)retrievedItems.forEach(item=>myListForm.addField(item));
 })
 
-LIST.addEventListener('change', (e)=>{
+myListForm.LIST.addEventListener('change', (e)=>{
     const tag = e.target;
-    if(tag.matches( 'input[type="text"]' )) updateValueInItem(tag);
-    else if(tag.matches('input[type="checkbox"]'))removeItem(tag);
+    if(tag.matches( 'input[type="text"]' )) myListForm.updateField(tag);
+    else if(tag.matches('input[type="checkbox"]'))myListForm.removeField(tag);
 })
 
 
